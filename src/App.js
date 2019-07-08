7ba46442c467f579862f3ec2ab0f2cf042d75a79
Tristan Guillevin <tristan.guillevin@gmail.com>
2019-07-08 17:59:49 +0200

    add log

--- src/App.js
+++ src/App.js
@@ -137,6 +137,7 @@ class AppComponent extends React.Component {
       // Get the first DataTable for our selected marks (usually there is just one)
       const worksheetData = sumdata;
 
+      console.log(worksheetData);
       const measures = worksheetData.columns.filter(
         columns =>
           columns.dataType === "float" || columns.dataType === "integer"
